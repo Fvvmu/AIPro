264857c5cc87c8b50e3fd75b8e7a24c36f08b501
Fvvmu <fvvmupvp@gmail.com>
2024-04-03 13:31:27 +0200

    Update AIPro

--- ai_app/models.py
+++ ai_app/models.py
@@ -1,6 +1,2 @@
-from django.db import models
-
-class ImageElement(models.Model):
-    title = models.CharField(max_length=255, blank=True)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="mediaphoto", blank=True, null=True)
+from models import ImageElement
+from models import TextElement
